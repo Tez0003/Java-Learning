053b18a8d0837449a01471347832727a3acd7cec
sravani <tns14@iitbbs.ac.in>
2023-05-29 21:00:09 +0530

    added a line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("Hey Sai")
+        System.out.print("Oota ayta?")
 
-        System.out.println("hii");
     }
 }
