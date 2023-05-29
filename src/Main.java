8711b03ed3442f110da7244d0b67287155bca7e4
sravani <tns14@iitbbs.ac.in>
2023-05-29 21:19:56 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey Sai")
-        System.out.print("Oota ayta?")
+        System.out.print("Oota")
+
+
 
     }
 }
